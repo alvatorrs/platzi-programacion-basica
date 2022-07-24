1fd4e2bcd670c532c547fcae589b3904d63e202e
alva_torrs <ramsabat.lml@gmail.com>
2022-07-23 21:29:20 -0500

    mokepones2

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,10 +1,34 @@
-console.log('Todo ok');
-
 // seleccionar mokepon
 
-function seleccionarMokeponJugador(){
-  alert('Seleccionaste tu Mokepon')
+//muestar el mokepon seleccionado una vez se de click al boton
+function seleccionarMokeponJugador() {
+  let mokepones = ['hipodoge', 'capipepo', 'ratigueya', 'langostelvis', 'tucapalma', 'pydos']
+  //si se selecciona una opcion
+  for (let mokepon of mokepones) {
+    let inputMokepon = document.getElementById(mokepon)
+    if (inputMokepon.checked) {
+      alert(`Seleccionaste a ${mokepon}`)
+    }
+  }
+
+  //si no se selecciona una opción
+  let contador = 0
+  for (let mokepon of mokepones) {
+    let inputMokepon = document.getElementById(mokepon)
+    if (inputMokepon.checked) {
+      contador += 1
+    }
+  }
+  if (contador === 0) {
+    alert('Seleccione una opción')
+  }
 }
 
-let botonMokeponJugador = document.getElementById('boton-mokepon') //seleccionando boton
-botonMokeponJugador.addEventListener('click', seleccionarMokeponJugador) //escuchado eventos
+//
+function iniciarJuego(){
+  let botonMokeponJugador = document.getElementById('boton-mokepon') //seleccionando boton
+  botonMokeponJugador.addEventListener('click', seleccionarMokeponJugador) //escuchado eventos
+}
+
+// agregando un escucha al navegador
+window.addEventListener('load', iniciarJuego)
