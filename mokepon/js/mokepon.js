14a9b423363e82c51c3cf0a50913fb467dda8759
alva_torrs <ramsabat.lml@gmail.com>
2022-07-25 22:59:45 -0500

    mokepones4

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,3 +1,7 @@
+// SELECCIONANDO MOKEPONES
+let mokepones = ['hipodoge', 'capipepo', 'ratigueya', 'langostelvis', 'tucapalma', 'pydos']
+
+
 //muestra el mokepon seleccionado una vez se de click al boton
 function seleccionarMokeponJugador() {
   let spanMokeponJugador = document.getElementById('mokepon-jugador')
@@ -6,7 +10,6 @@ function seleccionarMokeponJugador() {
     let inputMokepon = document.getElementById(mokepon)
     mokepon = mokepon.charAt(0).toUpperCase() + mokepon.slice(1) //capitalizando
     if (inputMokepon.checked) {
-      //alert(`Seleccionaste a ${mokepon}`)
       spanMokeponJugador.innerHTML = mokepon
     }
   }
@@ -20,8 +23,9 @@ function seleccionarMokeponJugador() {
   }
   if (contador === 0) {
     alert('Seleccione una opción')
+  } else {
+    seleccionarMokeponEnemigo()
   }
-  seleccionarMokeponEnemigo()
 }
 
 
@@ -34,23 +38,73 @@ function aleatorio(min, max) {
 // se llama justo después de seleccionarMokeponJugador
 function seleccionarMokeponEnemigo() {
   let spanMokeponEnemigo = document.getElementById('mokepon-enemigo') //trae el span
-  let ataqueAleatorio = aleatorio(0, mokepones.length -1) //valor aleatorio entre mokepones
-  let mokepon = mokepones[ataqueAleatorio]
+  let mokeponAleatorio = aleatorio(0, mokepones.length -1) //valor aleatorio entre mokepones
+  let mokepon = mokepones[mokeponAleatorio]
   mokepon = mokepon.charAt(0).toUpperCase() + mokepon.slice(1) //capitalizando
   spanMokeponEnemigo.innerHTML = mokepon
 }
 
 
+// SELECCIONANDO ATAQUES
+let ataques = ['FUEGO', 'AGUA', 'TIERRA']
+let ataqueJugador
+let ataqueEnemigo
+
+
+// insertar nuevos mensajes
+function crearMensaje() {
+  //elemento HTML donde se insertara el parrafo
+  let sectionMensajes = document.getElementById('mensajes')
+  //creando el parrafo
+  let parrafo = document.createElement('p') //elemento a crear
+  parrafo.innerHTML = `Tu mokepon atacó con ${ataqueJugador}, el mokepon de tu enemigo atacó con ${ataqueEnemigo} - PENDIENTE 🎉` //contenido del elemento
+  //ubicación del parrafo
+  sectionMensajes.appendChild(parrafo) //metiendo el parrafo en section con id="mensajes"
+}
+
+
+function ataqueJugadorFuego() {
+  ataqueJugador = 'FUEGO'
+  ataqueAleatorioEnemigo()
+} 
+
+
+function ataqueJugadorAgua() {
+  ataqueJugador = 'AGUA'
+  ataqueAleatorioEnemigo()
+} 
+
+
+function ataqueJugadorTierra() {
+  ataqueJugador = 'TIERRA'
+  ataqueAleatorioEnemigo()
+} 
+
+
+function ataqueAleatorioEnemigo() {
+  let ataqueAleatorio = ataques[Math.floor(Math.random()*ataques.length)]
+  ataqueEnemigo = ataqueAleatorio
+ 
+  crearMensaje()
+}
+
+
+
+
 // función de arranque del programa
-function iniciarJuego(){
+function iniciarJuego() {
+  //seleccionar mokepon
   let botonMokeponJugador = document.getElementById('boton-mokepon') //seleccionando boton
   botonMokeponJugador.addEventListener('click', seleccionarMokeponJugador) //escuchado eventos
+  //seleccionar ataque
+  let botonFuego = document.getElementById('boton-fuego')
+  botonFuego.addEventListener('click', ataqueJugadorFuego)
+  let botonAgua = document.getElementById('boton-agua')
+  botonAgua.addEventListener('click', ataqueJugadorAgua)
+  let botonTierra = document.getElementById('boton-tierra')
+  botonTierra.addEventListener('click', ataqueJugadorTierra)
 }
 
 
 /* Ejecución */
-
-let mokepones = ['hipodoge', 'capipepo', 'ratigueya', 'langostelvis', 'tucapalma', 'pydos']
-
-// agregando un escucha al navegador
-window.addEventListener('load', iniciarJuego)
+window.addEventListener('load', iniciarJuego) //agregando un escucha al navegador
